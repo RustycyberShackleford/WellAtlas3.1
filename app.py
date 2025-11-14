354600051e00968ac05cb3c1d17a255e971d8c7d
RustycyberShackleford <henrysuden@gmail.com>
2025-11-13 18:59:12 -0800

    Delete app.py

--- app.py
+++
@@ -1,7 +0,0 @@
-import os, sqlite3
-from flask import Flask,render_template
-app=Flask(__name__)
-DB=os.path.join(os.path.dirname(__file__),'wellatlas_demo.db')
-@app.route('/')
-def home(): return render_template('index.html')
-if __name__=='__main__': app.run()
